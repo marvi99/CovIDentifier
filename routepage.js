8fcd7007f8b00b0edbf21593181d72d1493bed0c
jlad7 <jlad7@uwo.ca>
2021-02-06 15:39:10 -0500

    Added Styling

--- routepage.js
+++ routepage.js
@@ -3,25 +3,42 @@ function initMap() {
   const directionsService = new google.maps.DirectionsService();
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
-    center: { lat: 37.77, lng: -122.447 },
+    center: { lat: 45.426020, lng: -75.681210 },
   });
-  var markerLatlng = new google.maps.LatLng(43.644402,-79.386414);
+  var markerLatlng = new google.maps.LatLng(45.412500,-75.673200);
   var marker = new google.maps.Marker({
     position: markerLatlng,
+    title:"Medium Risk Area"
+  });
+  var markerMintoLatlng = new google.maps.LatLng(45.420536,-75.677267);
+  var markerMinto = new google.maps.Marker({
+    position: markerMintoLatlng,
     title:"High Risk Area"
   });
   marker.setMap(map)
-
+  markerMinto.setMap(map)
   const cityCircle = new google.maps.Circle({
+    strokeColor: "#FFFF00",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FFFF00",
+    fillOpacity: 0.35,
+    map,
+    center: { lat: 45.412500, lng: -75.673200 },
+    radius: 250,
+  });
+
+  const MintoCircle = new google.maps.Circle({
     strokeColor: "#FF0000",
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: "#FF0000",
     fillOpacity: 0.35,
     map,
-    center: { lat: 43.644402, lng: -79.386414 },
-    radius: 50,
+    center: { lat: 45.420536, lng: -75.677267 },
+    radius: 350,
   });
+
 
   directionsRenderer.setMap(map);
   directionsRenderer.setPanel(document.getElementById('directionsPanel'));
@@ -35,8 +52,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
   const selectedMode = document.getElementById("mode").value;
   directionsService.route(
     {
-      origin: { lat: 43.6426, lng: -79.3871 },
-      destination: { lat: 43.643860, lng: -79.389290 },
+      origin: { lat: 45.426020, lng: -75.681210 },
+      destination: { lat: 45.403220, lng: -75.688260 },
+      provideRouteAlternatives: true,
       // Note that Javascript allows us to access the constant
       // using square brackets and a string value as its
       // "property."
